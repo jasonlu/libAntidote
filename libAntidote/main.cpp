73e2f55ac562df3c06154864017f7e3255c6d463
Jason Lu <jasonl.biz@gmail.com>
2014-10-23 18:49:58 -0400

    Initial Commit

---
+++ libAntidote/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libAntidote
+//
+//  Created by Jason Lu on 10/23/14.
+//  Copyright (c) 2014 BU MET HiLab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
